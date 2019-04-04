c83c05bd954ded1ec0c27a3667b48d908955cd54
Rogelio David Santacruz Amador <davidsantacruz09@gmail.com>
2019-04-03 19:06:29 -0600

    E

--- app/src/main/java/com/example/loginbuscar/Main2Activity.java
+++ app/src/main/java/com/example/loginbuscar/Main2Activity.java
@@ -1,13 +1,18 @@
 package com.example.loginbuscar;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
     public static String nombres= "names", username="user", password="pwd";
     TextView tvBienvenido, tvUser, tvPass;
+
+    FrameLayout buscar, registrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +31,34 @@ public class Main2Activity extends AppCompatActivity {
         tvBienvenido.setText("¡Bienvenido "+usuario+"!");
         tvUser.setText("Usuario: "+ nombreusuario);
         tvPass.setText("Contraseña: "+ contra);
+
+        registrar = findViewById(R.id.registrarUser);
+        buscar = findViewById(R.id.BuscarUser);
+
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent instancia = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(instancia);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        registrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    Intent instancia = new Intent(getApplicationContext(), Registro.class);
+                    startActivity(instancia);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
